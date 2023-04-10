a24df80edcdb43ea1cab309125d1c2b23fdff2f0
DiegoRx <dio351a@gmail.com>
2023-04-10 11:18:46 -0500

    ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,17 +1,26 @@
-module.exports = {
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+
+const AppDataSource = new DataSource({
   type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'root',
+  password: 'admin',
+  database: 'test',
   url: process.env.DATABASE_URL,
   synchronize: false,
   logging: false,
   migrations: ['src/database/migrations/*.ts'],
   migrationsTableName: 'migrations',
   entities: ['src/**/*.entity.ts'],
-  cli: {
-    migrationsDir: 'src/database/migrations',
-  },
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
-};
+});
+
+// to initialize initial connection with the database, register all entities
+// and "synchronize" database schema, call "initialize()" method of a newly created database
+// once in your application bootstrap
+AppDataSource.initialize()
+  .then(() => {
+    // here you can start to work with your database
+  })
+  .catch((error) => console.log(error));
