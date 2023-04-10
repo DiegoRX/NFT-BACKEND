361ee639b8d58c8ef7e5759ff84643d226d47953
DiegoRx <dio351a@gmail.com>
2023-04-10 09:15:56 -0500

    config

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,17 +1,29 @@
-module.exports = {
-  type: 'postgres',
-  url: process.env.DATABASE_URL,
-  synchronize: false,
-  logging: false,
-  migrations: ['src/database/migrations/*.ts'],
-  migrationsTableName: 'migrations',
-  entities: ['src/**/*.entity.ts'],
-  cli: {
-    migrationsDir: 'src/database/migrations',
-  },
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
-};
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+export class OrmConfig {
+  // Config TypeOrm
+  getConfig() {
+    const ormConfig: TypeOrmModuleOptions = {
+      type: 'postgres',
+      host: process.env.POSTGRESQL_HOST,
+      port: Number(process.env.POSTGRESQL_PORT) || 5432,
+      username: process.env.POSTGRESQL_USER || 'postgres',
+      password: process.env.POSTGRESQL_PASSWORD || '123456789',
+      database: process.env.POSTGRESQL_DB || 'potsgres',
+      entities: ['dist/**/*.entity{.js,.ts}'],
+      synchronize: false,
+      migrationsRun: true,
+      logging: false,
+      logger: 'file',
+      migrations: ['dist/database/migrations/**/*{.js, .ts}'],
+      ssl: true,
+      extra: {
+        // ssl: true
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+    };
+    return ormConfig;
+  }
+}
+export default new OrmConfig().getConfig();
