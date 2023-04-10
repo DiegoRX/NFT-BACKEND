4dc0cd654480e364a76f6568b94341718f28eaf8
DiegoRx <dio351a@gmail.com>
2023-04-10 09:55:05 -0500

    fixed

--- app/dist/auth/decorators/roles.decorator.d.ts
+++
@@ -1,3 +0,0 @@
-import { Role } from '../models/roles.models';
-export declare const ROLES_KEY = "roles";
-export declare const Roles: (...roles: Role[]) => import("@nestjs/common").CustomDecorator<string>;
