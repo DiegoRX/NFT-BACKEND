2192a04b3c4eaa9ebfdc8481f86d0520bc40e6b9
DiegoRx <dio351a@gmail.com>
2023-04-10 12:25:44 -0500

    new scripts

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,11 +3,6 @@ import { DataSource } from 'typeorm';
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'root',
-  password: 'admin',
-  database: 'test',
   url: process.env.DATABASE_URL,
   synchronize: false,
   logging: false,
